aed7a9f8052374111db033829c085b72cda2478c
田端佑也 <tabayu5@gmail.com>
2021-03-17 23:04:42 +0900

    add form component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import {AnswersList, Chats} from "./components";
+import FormDialog from './components/Forms/FormDialog';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class App extends React.Component {
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
 
@@ -35,6 +38,9 @@ export default class App extends React.Component {
     switch(true) {
       case (nextQuestionId === 'init'):
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
+        break;
+      case (nextQuestionId === 'contact'):
+        this.handleClickOpen()
         break;
       case (/^https:*/.test(nextQuestionId)):
         const a = document.createElemet('a');
@@ -58,6 +64,13 @@ export default class App extends React.Component {
     }
   }
 
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+      this.setState({ open: false });
+  };
 
   componentDidMount() {
     const initAnswer = "";
@@ -70,13 +83,14 @@ export default class App extends React.Component {
       scrollArea.scrollTop = scrollArea.scrollHeight;
     }
   }
-  
+
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
           <Chats chats={this.state.chats} />
           <AnswersList answers={this.state.answers} select={this.selectAnswer} />
+          <FormDialog open={this.state.open} handleClose={this.handleClose} />
         </div>
       </section>
     );
