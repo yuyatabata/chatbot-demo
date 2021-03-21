541b90244f0220a5c079655292a50e090481bea0
田端佑也 <tabayu5@gmail.com>
2021-03-21 22:33:24 +0900

    create api function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+const sendResponse = (response: functions.Response, statusCode: number, body: any) => {/* eslint-disable-line max-len */
+  response.send( {
+    statusCode,
+    body: JSON.stringify(body),
+  });
+};
+
+export const addDataset = functions.https.onRequest(async (req: any, res: any) =>{/* eslint-disable-line max-len */
+  if (req.method !== "POST") {
+    sendResponse(res, 405, {error: "invalid Request!"});
+  } else {
+    const dataset = req.body;
+    for (const key of Object.keys(dataset)) {
+      const data = dataset[key];
+      await db.collection("questions").doc(key).set(data);
+    }
+    sendResponse(res, 200, {message: "Successfully added dataset"});
+  }
+});
