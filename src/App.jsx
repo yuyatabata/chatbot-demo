b7cf388ae5cd8c22ff98bf73556a54c46e57c457
田端佑也 <tabayu5@gmail.com>
2021-03-23 23:16:34 +0900

    firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import {AnswersList, Chats} from "./components";
 import FormDialog from './components/Forms/FormDialog';
+import {db} from './firebase/index'
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,7 +12,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -72,9 +73,27 @@ export default class App extends React.Component {
       this.setState({ open: false });
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset})
+  }
+
   componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId)
+    (async() => {
+      const dataset = this.state.dataset
+
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+
+      this.initDataset(dataset)
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId)
+
+    })()
   }
 
   componentDidUpdate() {
